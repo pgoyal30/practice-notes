be2fb17a983e4778a0c73dfab9fa1f54f0479fca
Prince Goyal <princegoyal30p.g@gmail.com>
2025-07-07 15:27:43 +0530

    Tuple Function, Dictionary, Set, Practice Questions

---
+++ practice-6.py
@@ -0,0 +1,79 @@
+####################################################
+#  function to find maximum of three numbers 
+####################################################
+
+
+# def maximum_num(num1, num2, num3):
+#     if num1 > num2 and num1 > num3:
+#         return num1
+#     elif num2 > num1 and num2 > num3:
+#         return num2
+#     else:
+#         return num3
+    
+
+# # print(maximum_num(20, 40, 33))
+
+# print(maximum_num(40, 39, 23))
+
+
+
+####################################################
+# function to create and print a list where the values are square of numbers between 1 and 30
+####################################################
+
+
+# def create_list():
+#     l = []
+
+#     for i in range(1, 31):
+#         l.append(i**2)
+#     return l
+
+# print(create_list())
+
+
+
+############################################################
+# function which will check no. is prime or not 
+#############################################################
+
+
+
+# def check_prime(num):
+    
+#     if num == 1:
+#         print("It is not a prime number")
+
+#     elif num == 2:
+#         print("It is a prime number")
+
+#     elif  num > 2:
+#         for i in range(2, num):
+#             if num % i == 0:
+#                print("It is not a prime number")
+#                break
+#         else:
+#             print("it is a prime number")
+
+        
+# # check_prime(1)
+# # check_prime(2)
+# # check_prime(4)
+# check_prime(5)
+
+
+
+############################################################
+#  function to sum all the numbers in a list
+#############################################################
+
+# def add(numbers):
+#     total = 0
+
+#     for num in numbers:
+#         total += num
+
+#     return total
+
+# print(add([2,3,4,5,6,7]))
